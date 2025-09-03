b1db28ddd0d0f2033e1f18f93586440091393e1c
silasljennings <silasljennings@gmail.com>
2025-09-03 13:31:23 -0500

    updated curls

--- curls/health.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Health check endpoint (no authentication required)
-curl -X GET http://localhost:8080/health
--- curls/season.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Get current season information (no authentication required)
-curl -X GET http://localhost:8080/season
